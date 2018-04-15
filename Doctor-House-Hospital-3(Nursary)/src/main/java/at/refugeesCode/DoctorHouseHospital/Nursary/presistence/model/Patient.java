690c6a1e77b8933e04210946253480fa5c0045a9
Waelazar <waelazar@gmail.com>
2018-04-15 15:25:28 +0200

    add Patient model

--- Doctor-House-Hospital-3(Nursary)/src/main/java/at/refugeesCode/DoctorHouseHospital/Nursary/presistence/model/Patient.java
+++ Doctor-House-Hospital-3(Nursary)/src/main/java/at/refugeesCode/DoctorHouseHospital/Nursary/presistence/model/Patient.java
@@ -1,4 +1,61 @@
 package at.refugeesCode.DoctorHouseHospital.Nursary.presistence.model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+
+@Document
 public class Patient {
+
+    @Id
+    private String id;
+
+    private String name;
+
+    private List<String> symptoms;
+
+    private String illness;
+
+    public Patient() {
+    }
+
+    public Patient(String name, List<String> symptoms, String illness) {
+        this.name = name;
+        this.symptoms = symptoms;
+        this.illness = illness;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getSymptoms() {
+        return symptoms;
+    }
+
+    public void setSymptoms(List<String> symptoms) {
+        this.symptoms = symptoms;
+    }
+
+    public String getIllness() {
+        return illness;
+    }
+
+    public void setIllness(String illness) {
+        this.illness = illness;
+    }
 }
